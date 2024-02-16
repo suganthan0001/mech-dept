e65dd3150cd49a03cd4c9cb3f2609eed9e196573
suganthan0001 <suganthan0001@gmail.com>
2024-02-16 18:08:30 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import Head from "./Components/Head"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <Head /> */}
+    <App />
+  </React.StrictMode>
+);
